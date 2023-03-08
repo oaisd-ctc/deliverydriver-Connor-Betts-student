9d52d6f009bcc7a8990f65542b489a6fcbea8cc7
Connor Betts <112406724+Connor-Betts-student@users.noreply.github.com>
2023-03-08 09:44:49 -0500

    finished the funny

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,12 +6,14 @@ public class Driver : MonoBehaviour
 {
 
     [SerializeField] float steerSpeed = 0.5f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,5 +23,16 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Booster")
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
     }
 }
